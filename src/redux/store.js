9e0ad66a97ccf13167e1b014fa85721bc7970138
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-12-18 23:46:41 +0200

    start

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage/session';
 import contactsReducer from '../redux/reducers/contactsSlice';
 import filterReducer from '../redux/reducers/filterSlice';
 
